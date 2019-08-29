d2ead2d3b990dafc84109a485820adb27277cb04
Schuyler Reinken <sreinken3@gatech.edu>
2019-08-29 00:12:53 -0400

    Added Account Buttons

--- src/renderer/ui/AccountsPage/AccountsPage.tsx
+++ src/renderer/ui/AccountsPage/AccountsPage.tsx
@@ -1,21 +1,38 @@
 import * as React from "react"
-import { Theme, WithStyles, createStyles } from "@material-ui/core"
+import { Theme, WithStyles, createStyles, Paper, Button, Grid } from "@material-ui/core"
 import { withStyles } from "@material-ui/styles";
+import { fontStyle } from "@material-ui/system";
 
 const styles = (theme: Theme) => createStyles({
     root: {
-        "& h1": {
+        "& h1,h2": {
             boxSizing: "border-box",
             margin: "0",
-            padding: theme.spacing(1)
+            fontWeight: "normal",
         },
+        padding: theme.spacing(1),
         background: theme.palette.primary.dark,
         display: "flex",
+        flexDirection: "column",
         boxSizing: "border-box",
         width: "100vw",
         height: "100vh",
         color: "white",
-        fontFamily: "Roboto"
+        fontFamily: "Roboto",
+        fontStyle: "regular"
+    },
+    paper: {
+        borderRadius: "0",
+        display: "flex",
+        flexDirection: "row",
+        textAlign: "start",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+        background: theme.palette.primary.main,
+        padding: theme.spacing(1),
+        color: "white",
+        height: "100px",
+        width: "200px"
     }
 })
 
@@ -23,6 +40,17 @@ type IAccountPageComponent = WithStyles<typeof styles>
 export const AccountPageComponent: React.FunctionComponent<IAccountPageComponent> = ({ classes }) => (
     <div className={classes.root}>
         <h1>Accounts</h1>
+        <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} md={4}>
+                <Button className={classes.paper}><h2>USAA Main Checking</h2></Button>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+            <Button className={classes.paper}><h2>Skymiles Card</h2></Button>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+            <Button className={classes.paper}><h2>Uber Visa</h2></Button>
+            </Grid>
+        </Grid>
     </div>
 )
 
