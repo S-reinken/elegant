bf2018febc0a62693d6efd58a12d512d1d146090
Schuyler Reinken <xarlyle0@gmail.com>
2019-08-31 10:00:30 -0400

    Added prettierrc, Fixed title border and fixed Grid

--- src/renderer/ui/AccountsPage/AccountsPage.tsx
+++ src/renderer/ui/AccountsPage/AccountsPage.tsx
@@ -1,61 +1,84 @@
 import * as React from "react"
-import { Theme, WithStyles, createStyles, Paper, Button, Grid } from "@material-ui/core"
-import { withStyles } from "@material-ui/styles";
-import { fontStyle } from "@material-ui/system";
+import {
+  Theme,
+  WithStyles,
+  createStyles,
+  Paper,
+  Button,
+  Grid
+} from "@material-ui/core"
+import { withStyles } from "@material-ui/styles"
+import { fontStyle } from "@material-ui/system"
 
-const styles = (theme: Theme) => createStyles({
+const styles = (theme: Theme) =>
+  createStyles({
     root: {
-        "& h1,h4": {
-            display: "flex",
-            boxSizing: "border-box",
-            margin: "0",
-            fontWeight: "normal",
-        },
-        padding: theme.spacing(1),
-        background: theme.palette.primary.dark,
+      "& h1,h4": {
         display: "flex",
-        flexDirection: "column",
         boxSizing: "border-box",
-        width: "100vw",
-        height: "100vh",
-        color: "white",
-        fontFamily: "Roboto",
-        fontStyle: "regular"
+        margin: "0",
+        fontWeight: "normal"
+      },
+      padding: theme.spacing(1),
+      background: theme.palette.primary.dark,
+      display: "flex",
+      flexDirection: "column",
+      boxSizing: "border-box",
+      width: "100vw",
+      height: "100vh",
+      color: "white",
+      fontFamily: "Roboto",
+      fontStyle: "regular"
+    },
+    title: {
+      borderBottomStyle: "solid",
+      borderBottomColor: theme.palette.primary.light,
+      marginBottom: theme.spacing(1)
     },
     paper: {
-        borderRadius: "0",
-        display: "flex",
-        flexDirection: "column",
-        textAlign: "start",
-        justifyContent: "space-between",
-        alignItems: "flex-start",
-        background: theme.palette.primary.main,
-        padding: theme.spacing(1),
-        color: "white",
-        height: "100px",
-        width: "200px",
-        "&:hover": {
-            backgroundColor: theme.palette.primary.light
-        }
+      borderRadius: "0",
+      display: "flex",
+      flexDirection: "column",
+      textAlign: "start",
+      justifyContent: "space-between",
+      alignItems: "flex-start",
+      background: theme.palette.primary.main,
+      padding: theme.spacing(1),
+      color: "white",
+      height: "100px",
+      width: "200px",
+      "&:hover": {
+        backgroundColor: theme.palette.primary.light
+      }
     }
-})
+  })
 
 type IAccountPageComponent = WithStyles<typeof styles>
-export const AccountPageComponent: React.FunctionComponent<IAccountPageComponent> = ({ classes }) => (
-    <div className={classes.root}>
-        <h1>Accounts</h1>
-        <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} md={4}>
-                <Button className={classes.paper}><h4>USAA Main Checking</h4></Button>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <Button className={classes.paper}><h4>Skymiles Card</h4></Button>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <Button className={classes.paper}><h4>Uber Visa</h4></Button>
-            </Grid>
-        </Grid>
+export const AccountPageComponent: React.FunctionComponent<
+  IAccountPageComponent
+> = ({ classes }) => (
+  <div className={classes.root}>
+    <div className={classes.title}>
+      <h1>Accounts</h1>
     </div>
+    <Grid container spacing={2}>
+      <Grid item>
+        <Button className={classes.paper}>
+          <h4>USAA Main Checking</h4>
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button className={classes.paper}>
+          <h4>Skymiles Card</h4>
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button className={classes.paper}>
+          <h4>Uber Visa</h4>
+        </Button>
+      </Grid>
+    </Grid>
+  </div>
 )
 
 export const AccountPage = withStyles(styles)(AccountPageComponent)
