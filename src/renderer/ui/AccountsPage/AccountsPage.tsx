6c638e4fa026b8a9327022a9024a4197a07e6ce8
Schuyler Reinken <xarlyle0@gmail.com>
2019-09-01 09:53:21 -0400

    Refactored Styles out

--- src/renderer/ui/AccountsPage/AccountsPage.tsx
+++ src/renderer/ui/AccountsPage/AccountsPage.tsx
@@ -11,49 +11,7 @@ import { withStyles } from "@material-ui/styles"
 import { fontStyle } from "@material-ui/system"
 import { PageComponentProps } from "../App"
 import { Page } from "@/renderer/common/constants"
-
-const styles = (theme: Theme) =>
-  createStyles({
-    root: {
-      "& h1,h4": {
-        display: "flex",
-        boxSizing: "border-box",
-        margin: "0",
-        fontWeight: "normal"
-      },
-      padding: theme.spacing(1),
-      background: theme.palette.primary.dark,
-      display: "flex",
-      flexDirection: "column",
-      boxSizing: "border-box",
-      width: "100vw",
-      height: "100vh",
-      color: "white",
-      fontFamily: "Roboto",
-      fontStyle: "regular"
-    },
-    title: {
-      borderBottomStyle: "solid",
-      borderBottomColor: theme.palette.primary.light,
-      marginBottom: theme.spacing(1)
-    },
-    paper: {
-      borderRadius: "0",
-      display: "flex",
-      flexDirection: "column",
-      textAlign: "start",
-      justifyContent: "space-between",
-      alignItems: "flex-start",
-      background: theme.palette.primary.main,
-      padding: theme.spacing(1),
-      color: "white",
-      height: "100px",
-      width: "200px",
-      "&:hover": {
-        backgroundColor: theme.palette.primary.light
-      }
-    }
-  })
+import { styles } from "./Styles"
 
 interface IAccountPageComponent extends WithStyles<typeof styles> {
   itemClick: () => void
