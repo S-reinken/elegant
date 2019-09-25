5edf5f50e398bda8f1e4d2b6a20d447854cb9769
Schuyler Reinken <sreinken3@gatech.edu>
2019-09-24 20:49:31 -0400

    Got sqlite3 to run on a server at least

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,19 @@
+import * as sqlite3 from "sqlite3"
 const express = require( "express" );
 const app = express();
 const port = 8080; // default port to listen
 
+const connectToDB = () => {
+    let db = new sqlite3.Database("~/elegant.db", (err) => {
+        if (err) {
+            console.error(err.message)
+        }
+        console.log('Connected to the elegant database.');
+    })
+    console.log("Hello??")
+}
+
+connectToDB()
 // define a route handler for the default home page
 app.get( "/", ( req: any, res: any) => {
     res.send( { text: "Hello world!"} );
