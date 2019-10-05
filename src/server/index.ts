2a05b4b439556729819220cb0c08c19c7a1daa7b
Schuyler Reinken <sreinken3@gatech.edu>
2019-10-05 14:59:27 -0400

    Database now successfully updates with CSV import

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import * as sqlite3 from "sqlite3"
 import * as express from "express"
+import { map, forEach } from "lodash/fp"
 const app = express();
 app.use(express.json())
 const port = 8080; // default port to listen
@@ -17,8 +18,19 @@ app.get( "/", ( req: any, res: any) => {
     })
 } );
 
+type AmexTransaction = {
+    Date: string,
+    Description: string,
+    Category: string,
+    Amount: string
+}
+const makeInserts = map((obj: AmexTransaction) => `INSERT INTO transactions values("${obj.Date}", "NONE", "${obj.Description}", "${obj.Category}", "${obj.Amount}")`)
+
+
 app.post("/csv", (req, res) => {
-    console.log(`WHat does my request look like? :: ${req.body}`)
+    console.log(`What does my request look like? :: ${JSON.stringify(req.body)}`)
+    console.log(makeInserts(req.body))
+    forEach((q: string) => db.run(q), makeInserts(req.body))
 })
 
 // start the Express server
